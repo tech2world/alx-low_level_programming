aa7c9baf70ddbfc5ad2a686f7063261f0865ea2f
tech2world <abdulazeezibrahim847@gmail.com>
2022-12-11 12:31:19 -0800

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ * return: (0)
+ *
+ * this function uses printf to display function output
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return (0);
+}
