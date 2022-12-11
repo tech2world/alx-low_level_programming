c89a05f3c97e196232d91a32f9911a76126ab893
tech2world <abdulazeezibrahim847@gmail.com>
2022-12-11 10:00:42 -0800

    my fisrt c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the main function of the progran
+ * return - always return (0) success
+ *
+ * this fnction uses puts 
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
