7d0d9dca04e548f93aa9d627ba791e735d7d91d6
tech2world <abdulazeezibrahim847@gmail.com>
2022-12-12 08:59:50 -0800

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - beginning of tbe program
+ * description : print alphabets in lower case
+ *
+ * return 0 
+ */
+
+int main(void)
+{
+	int c = 'a'
+	
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n')
+	return (0)
+};
+	
