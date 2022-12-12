2e9a47e2fe2d4e97f7badc438649f6fbf4f064eb
tech2world <abdulazeezibrahim847@gmail.com>
2022-12-12 10:26:18 -0800

    while function

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in
+ * lowercase except q and e
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
