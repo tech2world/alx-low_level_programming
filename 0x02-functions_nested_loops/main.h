b7414df18ac01c140c2e3a9d3bbe6169945bee0e
tech2world <abdulazeezibrahim847@gmail.com>
2022-12-14 17:11:39 -0800

    compiled file 1 and 2

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef _main_h_
 #define _main_h_
 int _putchar(char);
-int print_alphabet(void);
-int print_alphabet_x10(void);
+void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
