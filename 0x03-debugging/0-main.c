95dce744f639c617bad981f9d08bde1bb9077fe4
tech2world <abdulazeezibrahim847@gmail.com>
2022-12-15 15:29:05 -0800

    debugging in C

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * main - test function to see if it prints -ve or +ve integer
+  *
+  *Return: 0
+  */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
