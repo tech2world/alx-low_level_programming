66a027a980bcbd0aa4b05df80ded80ff2065ce88
tech2world <abdulazeezibrahim847@gmail.com>
2022-12-17 04:55:33 -0800

    add func

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * add - addition of two integers
+  * @a: firat integer
+  * @b: second integer
+  * Description: takes two integers and return the addition
+  *
+  * Return: 0 always
+  */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
