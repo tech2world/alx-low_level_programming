cd3ccebaa854a7c5b0dbe69edc3761432a2ed641
tech2world <abdulazeezibrahim847@gmail.com>
2022-12-20 19:52:35 -0800

    print string

---
+++ 0x05-pointers_arrays_strings/6-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * puts2 - prints character of a string
+  * @str: the string
+  *
+  */
+void puts2(char *str)
+{
+	int index = 0;
+
+	len = 0;
+
+	while (str[index++])
+		len++;
+
+	for (index = 0; index < len; index += 2)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
