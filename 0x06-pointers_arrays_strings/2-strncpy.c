1a653bee02d40da7f36c9a206fa4d5cf01db6d61
tech2world <abdulazeezibrahim847@gmail.com>
2022-12-22 14:13:35 -0800

    copy file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,20 +3,28 @@
 /**
   * _strncpy - copies a string
   * @dest: first character
-  * @str: second character
+  * @src: second character
   * @n: value
   * Return: dest
   */
-char *_strncpy(char *dest, char *str, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
 	i = 0;
 	while (i < n && src[i] != '\0')
 	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		dest[i] = '\0';
+	}
+	while (i < n)
+	{
 		dest[i] = '\0';
 		i++;
 	}
-
 	return (dest);
 }
