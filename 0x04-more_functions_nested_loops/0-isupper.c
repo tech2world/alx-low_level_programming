01fe277d937a3255140158f6e1627b68d2c2826f
tech2world <abdulazeezibrahim847@gmail.com>
2022-12-29 04:55:23 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,15 @@
 /**
   * _isupper - checks if the input is in uppercase
   * @c: input param
-  * Return: 0 always
+  * Return: 1 if upper else 0
   */
 
 int _isupper(int c)
 {
 	if (c >= 65 && c > 91)
+	{
 		return (1);
+	}
 	else
 		return (0);
 }
