94946d90920dd80dbbd91722a0629a09cacd15a3
tech2world <abdulazeezibrahim847@gmail.com>
2023-01-01 14:20:17 -0800

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+  * main - finds and print the largest prime factor
+  * 
+  * Return: 0 always
+  */
+int main(void)
+{
+	long prime_num;
+	int divisor, prime_factor;
+
+
+	prime_num = 612852475143;
+
+	for (divisor = 2; prime_num > 1; divisor++)
+	{
+		while (prime_num % divisor == 0)
+		{
+			prime_num /= divisor;
+			prime_factor = divisor;
+		}
+	}
+
+	printf("%d\n", prime_factor);
+	return (0);
+}
