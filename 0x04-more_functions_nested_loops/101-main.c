2596d8f85e58adb0c87a42713a83f2bb4447eecc
tech2world <abdulazeezibrahim847@gmail.com>
2023-01-01 14:42:14 -0800

    print integers

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * main - check the code
+  *
+  * Return: 0 always
+  */
+
+int main(void)
+{
+	print_number(98);
+	_putchar('\n');
+	print_number(402);
+	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(-98);
+	_putchar('\n');
+	return (0);
+}
