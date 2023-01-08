7c13aabd65143c372cf843993b963cf47b93a247
tech2world <abdulazeezibrahim847@gmail.com>
2023-01-08 01:43:29 -0800

    print length of a string

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - check the code.
+  *
+  * Return: 0 alwaus.
+  */
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
+}
