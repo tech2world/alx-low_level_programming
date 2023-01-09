151145780bcf9143ab085683c886837699b73a05
tech2world <abdulazeezibrahim847@gmail.com>
2023-01-09 04:26:18 -0800

    argc and argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+  * main - a function that prints the no of arguments passed to it.
+  * @argc: argument count.
+  * @argv: argument vector (passed).
+  *
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	unsigned int i, sum, num;
+
+	sum = 0;
+
+	if (argc < 3)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	while (argc-- && argc > 0)
+	{
+		for (i = 0; argv[argc][i] != '\0'; i++)
+		{
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (0);
+			}
+		}
+		num = atoi(argv[argc]);
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (sum);
+}
