36a7f359f20de00e6fdd544b2e0bd99c393024e1
tech2world <abdulazeezibrahim847@gmail.com>
2023-01-18 14:01:05 -0800

    function pointrr

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,7 @@
   * print_name - prints a name using function pointer.
   * @name: inputted name.
   * @f: pointer to the print func.
- 	 */
+  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
