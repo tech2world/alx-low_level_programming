f0e8ea956fbea1972d6d2dcd07855d5978b5915c
tech2world <abdulazeezibrahim847@gmail.com>
2023-01-27 13:46:57 -0800

    singly linked

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", 0, "(nil)");
 		}
 		else
-			printf("[%d] %s\n", str->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		n_len++;
 	}
