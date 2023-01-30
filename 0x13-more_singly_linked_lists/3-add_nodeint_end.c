5cc70480e32d18452d53be8cfe7304f6b8f1a4af
tech2world <abdulazeezibrahim847@gmail.com>
2023-01-30 13:22:03 -0800

    more linked list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -25,9 +25,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (new);
 	}
 	current = *head;
+	
 	while (current->next != NULL)
 	{
-		current = currrnt->next;
+		current = current->next;
 	}
 	current->next = new;
 	return (new);
