4140408d302c096ff22bc366e54b9d5d694786aa
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-02-04 03:26:58 -0800

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,11 +10,12 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int set;
+    unsigned long int set;
 
-	if (index > (sizeof(unsigned long int) * 8 - 1))
-		return (-1);
-	set = 1 << index;
-	*n = *n | set;
+    if (index > (sizeof(unsigned long int) * 8 - 1))
+        return (-1);
+    set = 1 << index;
+    *n = *n | set;
+
     return (1);
 }
