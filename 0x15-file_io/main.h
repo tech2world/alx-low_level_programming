4887384814b558cc509a3613b60b58e0a59e863a
tech2world <abdulazeezibrahim847@gmail.com>
2023-02-07 06:08:07 -0800

    update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,7 +8,7 @@
 #include<fcntl.h>
 #include<unistd.h>
 
-int putchar(char c);
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
