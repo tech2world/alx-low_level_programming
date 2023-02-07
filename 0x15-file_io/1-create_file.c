0fe99b851a92e948c01295fb7ce89726eb7333f5
tech2world <abdulazeezibrahim847@gmail.com>
2023-02-07 07:16:23 -0800

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,24 +9,22 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, num_let, rwr;
+	int fd, num_let, rwr;
 
-    if (!filename)
-            return (-1);
+	if (!filename)
+		return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+		text_content = " ";
 
-    if (fd == -1)
-            return (-1);
-    if (!text_content)
-            text_content = " ";
-    
-    for (num_let = 0; text_content[num_let]; num_let++)
-            ;
-    rwr = write(fd, text_content, num_let);
-
-    if (rwr == -1)
-            return (-1);
-    close(fd);
-    return(1);
+	for (num_let = 0; text_content[num_let]; num_let++)
+		;
+	rwr = write(fd, text_content, num_let);
+	if (rwr == -1)
+		return (-1);
+	close(fd);
+	return (1);
 }
