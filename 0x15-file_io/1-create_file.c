7655880dd1f7539daf82a906f420aab91597b723
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-02-07 07:25:41 -0800

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,50 +9,29 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-<<<<<<< HEAD
-	int fd, num_let, rwr;
+	int fd;
+	int nletters;
+	int rwr;
 
 	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (fd == -1)
 		return (-1);
-	if (!text_content)
-		text_content = " ";
 
-	for (num_let = 0; text_content[num_let]; num_let++)
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
 		;
-	rwr = write(fd, text_content, num_let);
+
+	rwr = write(fd, text_content, nletters);
+
 	if (rwr == -1)
 		return (-1);
+
 	close(fd);
 	return (1);
 }
-=======
-        int fd, num_let, rwr;
-
-        if (!filename)
-                return (-1);
-
-        fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-        if (fd == -1)
-                return (-1);
-
-        if (!text_content)
-                text_content = "";
-
-        for (num_let = 0; text_content[num_let]; num_let++)
-                ;
-
-        rwr = write(fd, text_content, num_let);
-
-        if (rwr == -1)
-                return (-1);
-
-        close(fd);
-
-        return (1);
-}
->>>>>>> c88ae6ecf62113b18c8a829d0184f245f1cf4955
