b804d2d35f92bc63ec402d353ca39d75c163047d
tech2world <abdulazeezibrahim847@gmail.com>
2023-03-20 16:54:47 -0700

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/tech2world/alx-low_level_programming/blob/f915451363432e5eed759644789dee2040885326/0x18-dynamic_libraries/libtest.so
+wget -P /tmp https://github.com/tech2world/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
 export LD_PRELOAD=/tmp/libtest.so
