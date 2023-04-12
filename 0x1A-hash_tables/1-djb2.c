ab370dbac55a7233e6bc25b1bf60deb472456296
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-04-12 15:27:38 +0100

    Create 1-djb2.c

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Hash function implemening of the djb2 algorithm
+ * @str: The string used to generate hash value
+ * 
+ * Return: The calculated hash value
+*/
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+        unsigned long int hash;
+        int c;
+
+        hash = 5381;
+        while ((c = *str++))
+        {
+                hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+        }
+        return (hash);
+}
